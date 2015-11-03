51b872fc808ebf359882ea84604a08f0b20e9dac
binjaloo <rommelbrigaudit@rommels-mbp.home>
2015-11-03 15:28:56 -0800

    Initial Commit

---
+++ AWSDynamoLogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AWSDynamoLogin
+//
+//  Created by Rommel Brigaudit on 11/3/15.
+//  Copyright © 2015 Rommel Brigaudit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AWSDynamoLogin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AWSDynamoLogin
+//
+//  Created by Rommel Brigaudit on 11/3/15.
+//  Copyright © 2015 Rommel Brigaudit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
